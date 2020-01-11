6862e214938c809f3031ef32b4c2e4855c8dc770
mayrton <ayrtonmatthew@yahoo.com>
2020-01-11 15:06:44 -0700

    Updates

---
+++ precache-manifest.c9b5d1dddd12c823a9daebcd2ebc55e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b45f1f115ad5a4ec02d858f1209073af",
+    "url": "//mayrton1.github.io/clickygame/index.html"
+  },
+  {
+    "revision": "a40a26c686dc503b2639",
+    "url": "//mayrton1.github.io/clickygame/static/css/main.ab273c33.chunk.css"
+  },
+  {
+    "revision": "7cab17352a316dca7cc9",
+    "url": "//mayrton1.github.io/clickygame/static/js/2.f13001f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//mayrton1.github.io/clickygame/static/js/2.f13001f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a40a26c686dc503b2639",
+    "url": "//mayrton1.github.io/clickygame/static/js/main.7c474b74.chunk.js"
+  },
+  {
+    "revision": "86bb9b44c3447134c610",
+    "url": "//mayrton1.github.io/clickygame/static/js/runtime-main.8acd2e6e.js"
+  }
+]);
